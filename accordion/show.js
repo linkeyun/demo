28fcdbb716ed252a761c2d657262661ca6561a27
linkeyun <714997079@qq.com>
2021-05-19 23:47:54 +0800

    手风琴

---
+++ accordion/show.js
@@ -0,0 +1,89 @@
+imageList = {
+  imageList: [
+    {
+      name: '芈月',
+      images: {
+        smallUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/121/121.jpg',
+        largeUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/121/121-freehover.png'
+      },
+      smallWidth: 69,
+      smallHeight: 69,
+      largeWidth: 224,
+      largeHeight: 69
+    },
+    {
+      name: '老夫子',
+      images: {
+        smallUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/139/139.jpg',
+        largeUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/139/139-freehover.png'
+      },
+      smallWidth: 69,
+      smallHeight: 69,
+      largeWidth: 224,
+      largeHeight: 69
+    },
+    {
+      name: '程咬金',
+      images: {
+        smallUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/144/144.jpg',
+        largeUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/144/144-freehover.png'
+      },
+      smallWidth: 69,
+      smallHeight: 69,
+      largeWidth: 224,
+      largeHeight: 69
+    },
+    {
+      name: '露娜',
+      images: {
+        smallUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/146/146.jpg',
+        largeUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/146/146-freehover.png'
+      },
+      smallWidth: 69,
+      smallHeight: 69,
+      largeWidth: 224,
+      largeHeight: 69
+    },
+    {
+      name: '后羿',
+      images: {
+        smallUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/169/169.jpg',
+        largeUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/169/169-freehover.png'
+      },
+      smallWidth: 69,
+      smallHeight: 69,
+      largeWidth: 224,
+      largeHeight: 69
+    },
+    {
+      name: '鬼谷子',
+      images: {
+        smallUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/189/189.jpg',
+        largeUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/189/189-freehover.png'
+      },
+      smallWidth: 69,
+      smallHeight: 69,
+      largeWidth: 224,
+      largeHeight: 69
+    },
+    {
+      name: '杨玉环',
+      images: {
+        smallUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/176/176.jpg',
+        largeUrl: '//game.gtimg.cn/images/yxzj/img201606/heroimg/176/176-freehover.png'
+      },
+      smallWidth: 69,
+      smallHeight: 69,
+      largeWidth: 224,
+      largeHeight: 69
+    },
+  ],
+  showFirst: true,
+  onShow: true,
+  offset: false,
+  imageRadius: 5,
+  imageContainer: {
+    width: 698,
+    height: 69
+  }
+}
